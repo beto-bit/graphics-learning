6b6eddc3b2f9a1afb4a80780bb0def364fde2964
beto-bit <76757367+beto-bit@users.noreply.github.com>
2023-07-31 18:00:52 -0600

    Added helper functions

--- src/main.cpp
+++ src/main.cpp
@@ -23,6 +23,56 @@ constexpr auto vertexPath = "assets/vertex.glsl";
 constexpr auto fragmentPath = "assets/fragment.glsl";
 
 
+GLuint load_shaders_to_program(
+	const std::string& vertex_shader_src,
+	const std::string& fragment_shader_src
+) {
+	const char* vertex_shader_text = vertex_shader_src.c_str();
+	const char* fragment_shader_text = fragment_shader_src.c_str();
+
+	// Load Vertex Shader
+	GLuint vertex_shader = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(vertex_shader, 1, &vertex_shader_text, nullptr);
+	glCompileShader(vertex_shader);
+
+	// Load Fragment Shader
+	GLuint fragment_shader = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fragment_shader, 1, &fragment_shader_text, nullptr);
+	glCompileShader(fragment_shader);
+
+
+	// Attach shaders
+	GLuint program = glCreateProgram();
+	glAttachShader(program, vertex_shader);
+	glAttachShader(program, fragment_shader);
+	glLinkProgram(program);
+
+	return program;
+}
+
+
+static void key_callback(
+	GLFWwindow* window,
+	int key,
+	int scancode,
+	int action,
+	int mods
+) {
+	// Close on ESC press
+	if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+		glfwSetWindowShouldClose(window, true);
+}
+
+
+static void error_callback(
+	int error,
+	const char* description
+) {
+	std::cerr << "Error: " << description << '\n';
+}
+
+
+
 int main() {
 	// Init lib
 	if (!glfwInit()) return -1;
