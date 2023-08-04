cea7df44b55f521a18250f130355325e623500d0
beto-bit <76757367+beto-bit@users.noreply.github.com>
2023-08-04 07:14:00 -0600

    Not working triangle

--- src/main.cpp
+++ src/main.cpp
@@ -4,9 +4,28 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
+#include <array>
+#include <cmath>
 #include <iostream>
 
 #include "util/color.hpp"
+
+
+constexpr auto vertex_shader_src =
+"#version 330 core\n"
+"layout (location = 0) in vec3 aPos;\n"
+"void main()\n"
+"{\n"
+"	gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+"}";
+
+constexpr auto fragment_shader_src =
+"#version 330 core\n"
+"out vec4 FragColor;\n"
+"void main()\n"
+"{\n"
+"	FragColor = vec4(0.8f, 0.3f, 0.02f, 1.0f);\n"
+"}";
 
 
 static void key_callback(
@@ -30,10 +49,43 @@ static void error_callback(
 }
 
 
+GLuint compile_shader(GLuint shader_type, const char* shader_src) {
+	GLuint shader = glCreateShader(shader_type);
+
+	glShaderSource(shader, 1, &shader_src, nullptr);
+	glCompileShader(shader);
+
+	return shader;
+}
+
+GLuint create_shader_program(const char* vertex_shdr_src, const char* fragment_shdr_src) {
+	GLuint vertex_shader = compile_shader(GL_VERTEX_SHADER, vertex_shdr_src);
+	GLuint fragment_shader = compile_shader(GL_FRAGMENT_SHADER, fragment_shdr_src);
+
+	GLuint shader_program = glCreateProgram();
+
+	glAttachShader(shader_program, vertex_shader);
+	glAttachShader(shader_program, fragment_shader);
+	glLinkProgram(shader_program);
+
+	glDeleteShader(vertex_shader);
+	glDeleteShader(fragment_shader);
+
+	return shader_program;
+}
+
+
 int main() {
 	constexpr int window_heigth = 640;
 	constexpr int window_width = 480;
 	constexpr util::rgb_float bg_color(77u, 190u, 255u);
+
+	const std::array vertices {
+		-0.5f, -0.5f * std::sqrtf(3) / 3, 0.0f,
+		 0.5f, -0.5f * std::sqrtf(3) / 3, 0.0f,
+		 0.0f,  0.5f * std::sqrtf(3) * 2/3, 0.0f
+	};
+
 
 	// Init lib
 	if (!glfwInit()) return -1;
@@ -68,9 +120,33 @@ int main() {
 	// Visible area
 	glViewport(0, 0, window_heigth, window_width);
 
-	// Set color and render
-	glClearColor(bg_color.r, bg_color.g, bg_color.b, 1.0f);
-	glClear(GL_COLOR_BUFFER_BIT);
+
+	// Compile shaders
+	GLuint shader_program = create_shader_program(vertex_shader_src, fragment_shader_src);
+
+
+	// Vertex buffer object
+	GLuint VAO, VBO;
+
+	// Generate VAO and VBO with 1 object each
+	glGenVertexArrays(1, &VAO);
+	glGenBuffers(1, &VBO);
+
+	// Set this VAO as the current
+	glBindVertexArray(VAO);
+
+	// Upload the data into the buffers
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, vertices.size(), vertices.data(), GL_STATIC_DRAW);
+
+	// Configure the the Vertex Array so OpenGL knows how to read the VBO
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+
+	// Unbind
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	glBindVertexArray(0);
+
 
 	// Swap front and back buffers
 	glfwSwapBuffers(window);
@@ -78,9 +154,28 @@ int main() {
 	// Loop until the user closes the window
 	while (!glfwWindowShouldClose(window))
 	{
+		// Set color and render
+		glClearColor(bg_color.r, bg_color.g, bg_color.b, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		// Tell which shader program we want to use
+		glUseProgram(shader_program);
+		glBindVertexArray(VAO);
+
+		// Draw shit
+		glDrawArrays(GL_TRIANGLES, 0, 3);
+
+		// Swap front and back buffers
+		glfwSwapBuffers(window);
+
 		// Poll for and process events
 		glfwPollEvents();
 	}
+
+	// Delete all objects
+	glDeleteVertexArrays(1, &VAO);
+	glDeleteBuffers(1, &VBO);
+	glDeleteProgram(shader_program);
 
 	// Clean everything
 	glfwDestroyWindow(window);
